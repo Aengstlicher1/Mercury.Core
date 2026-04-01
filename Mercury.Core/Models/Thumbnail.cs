65571a8d29476dbc6d1a0cdf885ccde2b4926712
Aengstlicher1 <aengstlicher1@gmail.com>
2026-04-01 15:25:10 +0200

    Stuff

--- Mercury.Core/Models/Thumbnail.cs
+++ Mercury.Core/Models/Thumbnail.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,14 +10,9 @@ using System.Threading.Tasks;
 
 namespace Mercury.Core.Models
 {
-    public struct ThumbArray : IEnumerable
+    public struct ThumbArray(Thumbnail[] thumbnails) : IEnumerable
     {
-        public ThumbArray(Thumbnail[] thumbnails) 
-        {
-            Thumbs = thumbnails;
-        }
-
-        private Thumbnail[] Thumbs { get; set; }
+        private Thumbnail[] Thumbs { get; set; } = thumbnails;
 
         public Thumbnail HighestRes => Thumbs.MaxBy(t => t.Size.Height);
         public Thumbnail LowestRes => Thumbs.MinBy(t => t.Size.Height);
@@ -28,6 +24,30 @@ namespace Mercury.Core.Models
             return GetEnumerator();
         }
 
+        
+        public bool TryGetWithSize(Dimensions size, out Thumbnail thumbnail)
+        {
+            if (Thumbs.Any(t => t.Size.Equals(size)))
+            {
+                thumbnail = Thumbs.First(t => t.Size.Equals(size));
+                return true;
+            }
+            else
+            {
+                return LowestRes.TryGetCustomSize(size, out thumbnail);
+            }
+        }
+
+        public bool TryGetWithSize(Dimensions size, bool addToArray, out Thumbnail thumbnail)
+        {
+            bool success = TryGetWithSize(size, out thumbnail);
+            
+            if (success && addToArray)
+                Thumbs = Thumbs.Append(thumbnail).ToArray();
+            
+            return success;
+        }
+        
         public static ThumbArray Empty { get => new ThumbArray(); }
     }
 
@@ -47,7 +67,7 @@ namespace Mercury.Core.Models
         {
             return $"{Size} - {Url.Substring(0, 24)}...";
         }
-
+        
         public bool TryGetCustomSize(Dimensions size, out Thumbnail thumbnail)
         {
             thumbnail = new Thumbnail();
@@ -64,7 +84,7 @@ namespace Mercury.Core.Models
             }
             return isAdjustable;
         }
-
+        
         private static bool isAdjustableUrl(string url)
         {
             var regex = new Regex(@"=w\d+-h\d+-l\d+");
@@ -94,5 +114,8 @@ namespace Mercury.Core.Models
         {
             return $"{Width}x{Height}";
         }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+            =>  obj is Dimensions d && this.Width == d.Width && this.Height == d.Height;
     }
 }
